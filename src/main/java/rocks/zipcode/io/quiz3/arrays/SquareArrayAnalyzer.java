80fade767ca3339069fbfc8e0fdfb56108322c0b
Kate Bayard <katherinebayard@zipcoders-MacBook-Pro-8.local>
2019-03-11 10:32:21 -0400

    arrays completed

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -1,10 +1,24 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class SquareArrayAnalyzer {
     public static Boolean compare(Integer[] input, Integer[] squaredValues) {
-        return null;
+
+        Arrays.sort(input);
+        Arrays.sort(squaredValues);
+
+        boolean arraysMatch = false;
+
+        for (int i = 0; i < input.length; i++) {
+            if(Math.pow(input[i], 2) == squaredValues[i]) {
+                arraysMatch = true;
+            } else arraysMatch = false;
+        }
+
+        return arraysMatch;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -5,6 +5,33 @@ package rocks.zipcode.io.quiz3.arrays;
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        return null;
+
+        String[] wave = new String[str.length()];
+
+//        char[] temp = str.toCharArray();
+//        int count = 0;
+//
+//        for (int i = 0; i < str.length(); i++) {
+//
+//            for (Character c : temp) {
+//                if(Character.isLetter(c))
+//            }
+//            StringBuilder temp = new StringBuilder();
+//
+//        }
+
+
+
+
+        for (int i = 0; i < str.length(); i++) {
+            String p1 = str.substring(0, i).toLowerCase();
+            String p2 = str.substring(i,i+1).toUpperCase();
+            String p3 = str.substring(i+1).toLowerCase();
+
+
+            wave[i] = p1 + p2 + p3;
+        }
+
+        return wave;
     }
 }
